90cf62dbbac7d986d156f150513f3841e5d24a67
Tom Wardrop <tomwardrop@tommac.(none)>
2012-03-10 15:17:51 +1000

    Add config

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,7 @@ Sequel::Model.plugin :validation_helpers
 Sequel::Model.plugin :json_serializer
 Sequel::Model.raise_on_typecast_failure = false
 Sequel::Model.raise_on_save_failure = false
+Sequel::Model.json_serializer_opts[:naked] = true
 
 Sequel::Model.db = case Padrino.env
   when :production then
@@ -15,17 +16,17 @@ Sequel::Model.db = case Padrino.env
   when :development then
     Sequel.connect({
       adapter: 'tinytds',
-      host: 'trcsql02.trc.local',
-      user: 'TRC\tombstone_user',
-      password: '10Pippl$ah',
+      host: 'vm02',
+      user: 'VM02\administrator',
+      password: 'Passw0rd',
       database: 'Tombstone_Dev'
     })
   when :spec then
     Sequel.connect({
       adapter: 'tinytds',
-      host: 'trcsql02.trc.local',
-      user: 'TRC\tombstone_user',
-      password: '10Pippl$ah',
+      host: 'vm02',
+      user: 'VM02\administrator',
+      password: 'Passw0rd',
       database: 'Tombstone_Spec'
     })
 end
