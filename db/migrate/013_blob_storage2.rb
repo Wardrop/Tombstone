f1fbe6950e2797b7d90dce3ef6eb34734bb372a8
Tom Wardrop <TRC\tomw@vt004.trc.local>
2012-08-03 10:51:50 +1000

    Fixed migration data type.

--- db/migrate/013_blob_storage2.rb
+++ db/migrate/013_blob_storage2.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     alter_table :blob do
       drop_column :timestamp
       drop_column :enabled
-      add_column :exif, :varbinary, :size => :max
+      add_column :exif, :varchar, :size => :max
       add_column :modified_by, String, :size => 32
       add_column :modified_at, DateTime
     end
