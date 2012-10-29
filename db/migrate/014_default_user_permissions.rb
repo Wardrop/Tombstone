8cbd81b682d460fb57d1c8650f71b3ae14d7e305
Tom Wardrop <tomwardrop@mac01.local>
2012-10-29 11:11:09 +1000

    Added default user permissions to database migration.

---
+++ db/migrate/014_default_user_permissions.rb
@@ -0,0 +1,16 @@
+Sequel.migration do
+  up do
+    self[:user].insert(:id => 'marar', :role => 'coordinator')
+    self[:user].insert(:id => 'tomw', :role => 'coordinator')
+    self[:user].insert(:id => 'danielg', :role => 'coordinator')
+    self[:user].insert(:id => 'tatej', :role => 'coordinator')
+    self[:user].insert(:id => 'adrians', :role => 'coordinator')
+    self[:user].insert(:id => 'megank', :role => 'coordinator')
+    self[:user].insert(:id => 'amandar', :role => 'coordinator')
+    self[:user].insert(:id => 'ttu', :role => 'supervisor')
+  end
+  
+  down do
+    self[:user].delete
+  end
+end
